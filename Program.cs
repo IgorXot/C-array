646d8524fa21741086dfebf0938830b54642851b
Igor <igor.xot@mail.ru>
2022-12-28 01:24:03 +0300

    Lecsiyz next7

--- Program.cs
+++ Program.cs
@@ -914,6 +914,7 @@ Show2dArray(myArray);
 
  //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
+/*
 
 int[,] CreateRandom2dArray(int rows, int columns)
 {
@@ -966,6 +967,126 @@ int[,] myArray = CreateRandom2dArray(rows, columns);
 ShowArray(myArray);
 ShowAverage(AverageArray(myArray));
 
+*/
 
 
+//-------------------------------------------2dArray Next----------------------------------------------------------------
 
+// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+/*
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array) 
+
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ReverseArray(int[,] array)
+{
+    if(array.GetLength(0) == array.GetLength(1))
+        for(int i = 0; i < array.GetLength(0) - 1; i++)
+            for(int j = i + 1; j < array.GetLength(1); j++)
+            {
+                int temp = array[i,j];
+                array[i,j] = array [j,i];
+                array [j,i] = temp;
+            }
+    else
+        Console.WriteLine("Reverse is imposible");   
+} 
+
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+ReverseArray(myArray);
+Show2dArray(myArray);
+
+*/
+
+//Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] CreateRandom2dArray()
+{
+    Console.Write("Input a number of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a number of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a min possible value: ");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Input a max possible value: ");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + "\t");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SwitchRows(int[,] array, int row1, int row2)
+{
+    if(row1 != row2 && row1 < array.GetLength(0) && row2 < array.GetLength(0))
+    {
+        int temp = 0;
+        for(int j = 0;j < array.GetLength(1); j++)
+        {
+            temp = array[row1,j];
+            array[row1,j] = array[row2,j];
+            array[row2,j] = temp;
+
+        }
+    }
+}
+int[,] myArray = CreateRandom2dArray();
+Show2dArray(myArray);
+Console.Write("Введите номер первой строки: ");
+int numrow1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер второй строки: ");
+int numrow2 = Convert.ToInt32(Console.ReadLine());
+SwitchRows(myArray, numrow1, numrow2);
+Show2dArray(myArray);
+
+
+//Из двумерного массива целых чисел удалить строку и столбец, на пересечении которых расположен наименьший элемент.
